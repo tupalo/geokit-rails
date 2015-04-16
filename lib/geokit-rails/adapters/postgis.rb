b0095b786dd6ad369d22b6f034fd2c90eebb618d
Clemens Beer <clemens.beer@gmail.com>
2015-04-16 15:54:14 +0200

    PostGIS adapter derived from PostgreSQL adapter

---
+++ lib/geokit-rails/adapters/postgis.rb
@@ -0,0 +1,9 @@
+module Geokit
+  module Adapters
+    class PostGIS < PostgreSQL
+      #TODO: postgis native versions
+      # def sphere_distance_sql(lat, lng, multiplier)
+      # def flat_distance_sql(origin, lat_degree_units, lng_degree_units)
+    end
+  end
+end
